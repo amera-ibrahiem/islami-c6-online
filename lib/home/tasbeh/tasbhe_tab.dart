7cfc6cdce219b75cc3527aa3edd6949eb7da9d0e
princess amera <ameraibrahiem@outlook.com>
2022-08-04 10:13:47 +0200

    adding the sebha images and the word no of tasbeh

--- lib/home/tasbeh/tasbhe_tab.dart
+++ lib/home/tasbeh/tasbhe_tab.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class TasbehTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.brown,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            Image.asset('assets/images/head_sebha_logo.png'),
+            Padding(
+              padding: EdgeInsets.all(MediaQuery.of(context).size.height*.07),
+              child: Image.asset('assets/images/body_sebha_logo.png'),
+            ),
+          ],
+        ),
+        Text(AppLocalizations.of(context)!.noOfTasbeh,
+            textAlign: TextAlign.center,),
+      ],
     );
   }
 }
