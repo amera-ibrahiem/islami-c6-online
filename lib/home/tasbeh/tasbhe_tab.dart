90724a9da92eb19329ac62bdfb364ce6c514a533
princess amera <ameraibrahiem@outlook.com>
2022-08-04 11:57:03 +0200

    added rotation and counter of tasbeh and finished the tasbeh tab

--- lib/home/tasbeh/tasbhe_tab.dart
+++ lib/home/tasbeh/tasbhe_tab.dart
@@ -1,25 +1,92 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class TasbehTab extends StatelessWidget {
+class TasbehTab extends StatefulWidget {
+  @override
+  State<TasbehTab> createState() => _TasbehTabState();
+}
+
+class _TasbehTabState extends State<TasbehTab> {
+  int counter = 0;
+  List<String> tasbeh =['سبحان الله','الحمد لله','لا اله الا الله','الله اكبر','لا حول و لا قوة الا بالله'];
+  int index=0;
+  double angle=0;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Stack(
-          alignment: Alignment.topCenter,
+    return SizedBox(
+      width: double.infinity,
+      child:
+        Column(
           children: [
-            Image.asset('assets/images/head_sebha_logo.png'),
-            Padding(
-              padding: EdgeInsets.all(MediaQuery.of(context).size.height*.07),
-              child: Image.asset('assets/images/body_sebha_logo.png'),
+            Stack(
+              alignment: Alignment.topCenter,
+              children: [
+                Image.asset('assets/images/head_sebha_logo.png'),
+                InkWell(
+                  onTap: (){
+                    onTasbehTab();
+                  },
+                  child: Padding(
+                    padding: EdgeInsets.all(MediaQuery.of(context).size.height*.07),
+                    child: Transform.rotate(
+                        angle: angle,
+                        child: Image.asset('assets/images/body_sebha_logo.png')),
+                  ),
+                ),
+              ],
             ),
+            Text(AppLocalizations.of(context)!.noOfTasbeh,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
+                ),),
+            SizedBox(height: 20,),
+            Container(
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: Color(0xffB7935F),
+                borderRadius:BorderRadius.circular(20)
+              ),
+              child: Text(
+                '$counter',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                  color: Color(0xffB7935F),
+                  borderRadius:BorderRadius.circular(30)
+              ),
+              child: Text(
+                '${tasbeh[index]}',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold
+                ),
+              ),
+            )
           ],
         ),
-        Text(AppLocalizations.of(context)!.noOfTasbeh,
-            textAlign: TextAlign.center,),
-      ],
     );
+  }
+  void onTasbehTab(){
+    counter++;
+    if(counter%33==0){
+      index++;
+    }
+    if(index==tasbeh.length-1){
+      index=0;
+    }
+    angle+= 1/33;
+    setState((){});
   }
 }
